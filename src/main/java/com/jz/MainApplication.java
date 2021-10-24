5686416d2cb87783c63ab14f4dcc4bc87f88a2a5
Jacek Zalewski <jacekzalewski95@gmail.com>
2021-10-24 14:15:28 +0200

    Init project

---
+++ src/main/java/com/jz/MainApplication.java
@@ -0,0 +1,10 @@
+package com.jz;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+	public static void main(String[] args) {
+
+	}
+}
